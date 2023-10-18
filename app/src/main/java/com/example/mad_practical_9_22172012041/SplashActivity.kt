3789ff51308484706476fa9b008642d864fad651
student <student@gnu.ac.in>
2023-10-18 08:55:40 +0530

    18-10

--- app/src/main/java/com/example/mad_practical_9_22172012041/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_22172012041/SplashActivity.kt
@@ -1,32 +1,56 @@
 package com.example.mad_practical_9_22172012041
 
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 
-class SplashActivity : AppCompatActivity() {
-    lateinit var alarmanimation: AnimationDrawable
+class SplashActivity : AppCompatActivity() ,AnimationListener{
+    lateinit var logoanimation: AnimationDrawable
+    lateinit var tweenanimation:Animation
+    var logoimg: ImageView = findViewById(R.id.logo)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        var logoimg: ImageView = findViewById(R.id.logo)
-        logoimg.setBackgroundResource(R.drawable.alarm_animation_list)
-        alarmanimation = logoimg.background as AnimationDrawable
+
+        logoimg.setBackgroundResource(R.drawable.logo_animation_list)
+        logoanimation = logoimg.background as AnimationDrawable
+
+        tweenanimation = AnimationUtils.loadAnimation(this,R.anim.tween_animation)
+        tweenanimation.setAnimationListener(this)
 
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         if(hasFocus)
         {
-            alarmanimation.start()
+           logoimg.startAnimation(tweenanimation)
+
         }
         else
         {
-            alarmanimation.stop()
+            logoanimation.stop()
         }
         super.onWindowFocusChanged(hasFocus)
+    }
+
+    override fun onAnimationStart(p0: Animation?) {
+
+    }
+
+    override fun onAnimationEnd(p0: Animation?) {
+        //intent
+        Intent(this,MainActivity::class.java).also { startActivity(it) }
+    }
+
+    override fun onAnimationRepeat(p0: Animation?) {
+
     }
 
 }
